f8d0c97b581443fa390b883552e9c0ca496b3f4f
ranwahle <ran.wahle@gmail.com>
2019-02-28 00:48:57 +0200

    Remove www build

--- projects/stencil-components-gallery/stencil.config.ts
+++ projects/stencil-components-gallery/stencil.config.ts
@@ -7,9 +7,5 @@ export const config: Config = {
   outputTargets:[
     { type: 'dist' },
     { type: 'docs' },
-    {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
+     ]
 };
